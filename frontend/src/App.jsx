50a21b09e6d8a6f4620580cbee74e3a8a2c01a95
Bao Le <lgbaowork05@gmail.com>
2025-06-19 21:22:01 +0700

    login frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,47 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import "./App.scss";
+import LoginPage from "./pages/LoginPage";
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const handleLoginWithGoogle = () => {
+//     window.location.href =
+//       "http://localhost:5000/api/auth/google"; // Adjust the URL to your backend endpoint
+//   };
+//   return (
+//     <>
+//       <div>
+//         <div>
+//           <button onClick={handleLoginWithGoogle}>Sign in with Google</button>
+//         </div>
+//         <a href="https://vite.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   );
+// }
 function App() {
-  const [count, setCount] = useState(0);
-  const handleLoginWithGoogle = () => {
-    window.location.href =
-      "http://localhost:5000/api/auth/google"; // Adjust the URL to your backend endpoint
-  };
   return (
-    <>
-      <div>
-        <div>
-          <button onClick={handleLoginWithGoogle}>Sign in with Google</button>
-        </div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage/>} />
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
