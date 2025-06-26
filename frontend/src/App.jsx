f35348982f6178524c02707385eba9168d6b4e41
Hieu Vo <volengochieu1602@gmail.com>
2025-06-26 16:10:42 +0700

    User profile interface (unfinished)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.scss";
 import LoginPage from "./pages/LoginPage";
 import Signuppage from "./pages/SignupPage";
 import HomePage from './pages/HomePage';
+import UserProfile from './pages/UserProfile'
+
 function App() {
   return (
     <Router>
@@ -10,6 +12,7 @@ function App() {
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/signup" element={<Signuppage/>} />
         <Route path="/" element={<HomePage/>} />
+        <Route path="/profile" element={<UserProfile/>} />
       </Routes>
     </Router>
   );
--- frontend/src/components/HeaderBar.jsx
+++ frontend/src/components/HeaderBar.jsx
@@ -92,7 +92,9 @@ const HeaderBar = () => {
                 {/* Dropdown Menu */}
                 <div className={`navigation-bar ${navOpen ? "active" : ""}`}>
                   <button className="dropdown-item">Account</button>
-                  <button className="dropdown-item">Profile</button>
+                  <button className="dropdown-item"
+                    onClick={() => navigate("/profile")}
+                  >Profile</button>
                   <button className="dropdown-item">Upgrade to Premium</button>
                   <button className="dropdown-item">Support</button>
                   <button className="dropdown-item">Download</button>
